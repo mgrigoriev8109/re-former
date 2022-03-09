45d67a3d6b5589afd16a3e8418c17b8516738b0b
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-09 09:43:33 -0500

    Finish User View's HTML Form
    Using whitelisted_user_params in the UserController's #create action, which redirects to new, not create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
+require 'debug'
 class UsersController < ApplicationController
 	def new
 		
 	end
 
 	def create
-		@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-	
+		@user = User.new(whitelisted_user_params)
+
 		if @user.save
 			redirect_to new_user_path
 		else
 			render :new
 		end
+	end
+
+	private 
+
+	def whitelisted_user_params
+		params.require(:user).permit(:username,:email,:password)
 	end
 end
