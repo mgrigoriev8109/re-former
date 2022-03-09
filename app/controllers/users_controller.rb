a8f0bca2b4ea0f5b68abc982ce90ad7063047309
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-09 12:01:16 -0500

    Modify User's new view to #form_with
    Using value for prepopulated input, and placeholder for default input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 require 'debug'
 class UsersController < ApplicationController
 	def new
-		
+		@user = User.new()
 	end
 
 	def create
-		@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-		#@user = User.new(whitelisted_user_params)
+		#@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+		@user = User.new(whitelisted_user_params)
 
 		if @user.save
 			redirect_to new_user_path
